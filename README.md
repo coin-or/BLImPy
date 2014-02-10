a72658750f83e55eb4383ca9a10ed57894be8cd0
Ted Ralphs <ted@lehigh.edu>
2014-02-10 11:33:00 -0500

    Adding some verbiage to the README and correcting a typo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 BLImPy
 ======
+
+BLImPy (Basic List Implementation in Python) is a basic implementation of a
+linked list data structure that is a drop-in replacement for the Python list.
+It supports all basic list methods. The data structure is not as efficient as
+the Python list for many operations and in most cases, should not be used
+except in a teaching setting. It was developed to illustrate basic principles
+and to be used with GiMPy, a graph implementation in Python. BLImPy includes a
+stack data structure, a queue data structure, and a priority queue data
+structure.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 setup(name='coinor.blimpy',
       version='1.1.0',
       description='Basic List Implementation in Python',
-      long_description='BLiMPy (Basic List Implementation in Python) is a basic implementation of a linked list data structure that is a drop-in replacement for the Python list. It supports all basic list methods. The data structure is not as efficient as the Python list for many operations and in most cases, should not be used except in a teaching setting. It was developed to illustrate basic principles and to be used with GiMPy, a graph implementation in Python. BLImPy includes a stack data structure, a queue data structure, and a priority queue data structure.',
+      long_description='BLImPy (Basic List Implementation in Python) is a basic implementation of a linked list data structure that is a drop-in replacement for the Python list. It supports all basic list methods. The data structure is not as efficient as the Python list for many operations and in most cases, should not be used except in a teaching setting. It was developed to illustrate basic principles and to be used with GiMPy, a graph implementation in Python. BLImPy includes a stack data structure, a queue data structure, and a priority queue data structure.',
       author='Aykut Bulut, Ted Ralphs',
       author_email='{aykut,ted}@lehigh.edu',
       license='Eclipse Public License',
